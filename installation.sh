ff0e9e91f45a1b3a060a0228b8d858bff61baac3
VChowdary351 <chowdary7699@gmail.com>
2024-08-13 14:44:22 +0530

    k8s cluster

--- installation.sh
+++ installation.sh
@@ -1,40 +1,55 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
         exit 1
     else
-        echo -e "$2...$G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-if [ $USERID -ne 0 ]
+if [ $ID -ne 0 ]
 then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-    echo "You are super user."
-fi
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-# docker
 yum install -y yum-utils
+VALIDATE $? "Installed yum utils"
+
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+VALIDATE $? "Added docker repo"
+
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+VALIDATE $? "Installed docker components"
+
 systemctl start docker
+VALIDATE $? "Started docker"
+
 systemctl enable docker
+VALIDATE $? "Enabled docker"
+
 usermod -aG docker ec2-user
-VALIDATE $? "Docker installation"
+VALIDATE $? "added ec2-user to docker group"
+echo -e "$R Logout and login again $N"
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
